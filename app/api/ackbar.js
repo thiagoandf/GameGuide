0ffd46d0c3fb1bf14f6cf46272ed371c0344d3b6
Caio Rulli <caiorulli@gmail.com>
2018-05-07 23:55:53 -0300

    WIP gamelist integration

--- app/api/ackbar.js
+++ app/api/ackbar.js
@@ -1,26 +1,38 @@
+import request from 'utils/request';
+
+// export const getGameList = () =>
+//   Promise.resolve([
+//     {
+//       id: 1,
+//       name: 'Tokaido',
+//     },
+//     {
+//       id: 2,
+//       name: '7 Wonders',
+//     },
+//     {
+//       id: 3,
+//       name: 'Takenoko',
+//     },
+//     {
+//       id: 4,
+//       name: '7 Wonders Duel',
+//     },
+//     {
+//       id: 5,
+//       name: 'Hanabi',
+//     },
+//   ]);
+
+const ackbarUrl = process.env.ackbarUrl;
+
 export const getGameList = () =>
-  Promise.resolve([
-    {
-      id: 1,
-      name: 'Tokaido',
-    },
-    {
-      id: 2,
-      name: '7 Wonders',
-    },
-    {
-      id: 3,
-      name: 'Takenoko',
-    },
-    {
-      id: 4,
-      name: '7 Wonders Duel',
-    },
-    {
-      id: 5,
-      name: 'Hanabi',
-    },
-  ]);
+  request(`${ackbarUrl}/api/Games`, {
+    method: 'GET',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+    }),
+  });
 
 export const postLikeGame = () =>
   Promise.resolve();
