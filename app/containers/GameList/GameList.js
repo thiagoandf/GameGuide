ba96aea9207ea26c047a83285aaedbc78d442701
Thiago de Andrade Feliciano <thiagoandf@gmail.com>
2018-05-09 22:20:50 -0300

    Finished GameList, working both on mobile and desktop

--- app/containers/GameList/GameList.js
+++ app/containers/GameList/GameList.js
@@ -37,12 +37,28 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'center',
     width: '100%',
+    height: '100%',
   },
   gameStyle: {
-    margin: '20px',
-    maxWidth: '300px',
+    padding: '5%',
+    marginTop: '20px',
+    minWidth: '100%',
     textAlign: 'center',
     align: 'center',
+  },
+  gridList: {
+    minWidth: '70%',
+    maxWidth: '70%',
+    fullWidth: true,
+    height: 'auto',
+  },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
   },
 };
 
@@ -60,22 +76,25 @@ class GameList extends React.Component {
               <FlatButton label="Recommendations" onClick={this.props.goToRecommendations} style={styles.buttonStyle} />
               <FlatButton icon={<PowerSettingsNew />} onClick={this.props.logout} style={styles.buttonStyle} />
             </AppBar>
-            <GridList
-              cellHeight={180}
-              style={{ alignItems: 'center' }}
-            >
-              {this.props.gameList.map((game) => (
-                <GridTile
-                  key={game.id}
-                  title={game.name}
-                  actionIcon={<IconButton><ThumbUp color="white" onClick={() => this.props.likeGame(game.id)} /></IconButton>}
-                  style={styles.gameStyle}
-                  onClick={() => this.props.goToGameDetail(game.id)}
-                >
-                  <img src={TokaidoImage} alt="cool game" />
-                </GridTile>
-              ))}
-            </GridList>
+            <div style={styles.root}>
+              <GridList
+                cellHeight={200}
+                cellWidth={'100%'}
+                style={styles.gridList}
+              >
+                {this.props.gameList.map((game) => (
+                  <GridTile
+                    key={game.id}
+                    title={game.name}
+                    actionIcon={<IconButton><ThumbUp color="white" onClick={() => this.props.likeGame(game.id)} /></IconButton>}
+                    style={styles.gameStyle}
+                    onClick={() => this.props.goToGameDetail(game.id)}
+                  >
+                    <img src={TokaidoImage} alt="cool game" />
+                  </GridTile>
+                ))}
+              </GridList>
+            </div>
           </div>
         </div>
       </MuiThemeProvider>
