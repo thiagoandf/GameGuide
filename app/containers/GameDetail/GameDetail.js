97c25274972bb6f9079188724f38edf59c9dbfac
Caio Rulli <caiorulli@gmail.com>
2018-05-20 16:12:28 -0300

    GameDetail improvements

--- app/containers/GameDetail/GameDetail.js
+++ app/containers/GameDetail/GameDetail.js
@@ -1,40 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { RaisedButton } from 'material-ui';
+import {
+  Card,
+  CardMedia,
+  CardTitle,
+  CardText,
+  CardActions,
+  FlatButton,
+} from 'material-ui';
 import VerticalContainer from '../../components/VerticalContainer';
 import MainAppBar from '../../components/MainAppBar';
-
-const styles = {
-  buttonStyle: {
-    backgroundColor: 'transparent',
-    color: 'white',
-    fontWeight: 50,
-    paddingTop: 12,
-    height: 40,
-  },
-  notShownButton: {
-    display: 'none',
-  },
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-  },
-  imageDiv: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    paddingTop: 100,
-  },
-  gameImage: {
-    maxWidth: 400,
-    height: 'auto',
-  },
-};
 
 class GameDetail extends React.Component {
   componentDidMount() {
@@ -50,26 +25,24 @@ class GameDetail extends React.Component {
             logout={this.props.logout}
           />
           <VerticalContainer>
-            <div style={styles.imageDiv}>
-              <div>
-                <img
-                  src={this.props.game.coverImage}
-                  alt="cool game"
-                  style={styles.gameImage}
-                />
-              </div>
-            </div>
-            <div>
-              <h1>{this.props.game.name}</h1>
-              <p>{this.props.game.year}</p>
-              <p>
-                {this.props.game.numberOfPlayers} players -{' '}
-                {this.props.game.duration}
-              </p>
-              <p>Recommended age: {this.props.game.age}</p>
-              <p>{this.props.game.description}</p>
-              <RaisedButton label="Go Back" onClick={this.props.goBack} />
-            </div>
+            <Card>
+              <CardMedia>
+                <img src={this.props.game.coverImage} alt="cool game" />
+              </CardMedia>
+              <CardTitle
+                title={this.props.game.name}
+                subtitle={this.props.game.year}
+              />
+              <CardText>
+                Para {this.props.game.numberOfPlayers} jogadores
+              </CardText>
+              <CardText>Duração: {this.props.game.duration} minutos</CardText>
+              <CardText>Idade recomendada: {this.props.game.age}</CardText>
+              <CardText>{this.props.game.description}</CardText>
+              <CardActions>
+                <FlatButton label="Voltar" onClick={this.props.goBack} />
+              </CardActions>
+            </Card>
           </VerticalContainer>
         </div>
       </VerticalContainer>
@@ -93,6 +66,12 @@ GameDetail.propTypes = {
   goToGameList: PropTypes.func.isRequired,
   goToRecommendations: PropTypes.func.isRequired,
   logout: PropTypes.func.isRequired,
+};
+
+GameDetail.defaultProps = {
+  game: {
+    coverImage: '',
+  },
 };
 
 export default GameDetail;
