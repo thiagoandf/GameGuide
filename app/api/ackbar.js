e9cdcd1bf04631ac0e54bdc9226783648f8e04ae
Caio Rulli <caiorulli@gmail.com>
2018-09-28 21:59:07 -0300

    Added new ackbar endpoints

--- app/api/ackbar.js
+++ app/api/ackbar.js
@@ -21,8 +21,29 @@ const requestAckbar = (path, method, payload) => {
 
 export const getGameList = () => requestAckbar('api/Games', 'GET');
 
+export const postPlayerSignup = (
+  email,
+  password,
+  avatarUrl,
+  collectionSize,
+  weeklyPlayTime,
+) =>
+  requestAckbar('api/Player/Signup', 'POST', {
+    email,
+    password,
+    avatarUrl,
+    collectionSize,
+    weeklyPlayTime,
+  });
+
+export const postViewGame = (gameId, token) =>
+  requestAckbar(`api/Player/ViewGame/${gameId}`, 'POST', { token });
+
 export const postLikeGame = (gameId, token) =>
   requestAckbar(`api/Player/LikeGame/${gameId}`, 'POST', { token });
+
+export const postOwnGame = (gameId, token) =>
+  requestAckbar(`api/Player/OwnGame/${gameId}`, 'POST', { token });
 
 export const getRecommendations = token =>
   requestAckbar('api/Player/Recommendations', 'GET', { token });
@@ -30,13 +51,21 @@ export const getRecommendations = token =>
 export const postLogin = (email, password) =>
   requestAckbar('api/User/Login', 'POST', { email, password });
 
+export const getReportUrls = token =>
+  requestAckbar('api/Customer/GetReportUrl', 'GET', { token });
+
+// deprecated
 export const postSignup = (email, password) =>
   requestAckbar('api/User/Signup', 'POST', { email, password });
 
 export default {
   getGameList,
+  postPlayerSignup,
+  postViewGame,
   postLikeGame,
+  postOwnGame,
   getRecommendations,
   postLogin,
+  getReportUrls,
   postSignup,
 };
